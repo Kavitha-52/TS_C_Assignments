aa2cee3bd7619239bfbfe847fe9702f4b1357d61
Kavitha-52 <98574222+Kavitha-52@users.noreply.github.com>
2022-06-13 19:14:41 +0530

    r+mode_str

---
+++ files/r+mode_str.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+
+FILE *fp=fopen("string.txt","w+");// open a new file using file pointer
+if(fp)//check if file is created or not, fp!=null creates a new file
+printf("file opened successfully\n");
+else
+printf("file opened not successfully\n");//if fp=null file opened not successfullly
+
+char str[20]="thundersoft";
+char str1[20];
+fputs(str,fp);
+rewind(fp);
+fgets(str1,20,fp);
+printf("%s\n",str1);
+fclose(fp); // file is close
+}
